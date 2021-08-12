1229f193cfbd0648a1705615e1490990c9861ac5
Vedank022 <vedankn01@gmail.com>
2021-08-13 02:34:43 +0530

    routing

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-post-edit',
   templateUrl: './post-edit.component.html',
-  styleUrls: ['./post-edit.component.css']
+  styleUrls: ['./post-edit.component.css'],
 })
 export class PostEditComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null),
+      paragraph: new FormControl(null),
+      imgPath: new FormControl(null),
+    });
   }
 
+  onSubmit() {}
 }
